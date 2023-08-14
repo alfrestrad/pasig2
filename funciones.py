a269c65ed56bf202a43e3dbd6f9718b9f812d6f2
alfrestrad <alfredoestrad11@gmail.com>
2023-08-14 07:45:05 -0600

    Mi primer programa

---
+++ funciones.py
@@ -0,0 +1,13 @@
+def suma():# parametros (variables que recibe la función) argumento es el valor que reciben
+    a = 4
+    b = 7
+    c = a + b
+    print(c)
+    
+suma()
+
+def suma(a,b):
+    c = a + b
+    return c
+c = suma(20,4)
+print(c)
